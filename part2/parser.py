1dc0eafbb57c7c10b80c1c8f0e07f3ea73715c49
marychka2335 <marychka2335@icloud.com>
2024-04-06 20:44:15 +0300

    part1 commit

---
+++ part2/parser.py
@@ -0,0 +1,41 @@
+class ParsingError(Exception):
+    pass
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise ParsingError('Помилка синтаксичного аналізу')
+
+    def eat(self, token_type):
+        """
+        Порівнюємо поточний токен з очікуваним токеном і, якщо вони збігаються,
+        'поглинаємо' його й переходимо до наступного токена.
+        """
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def term(self):
+        """ Парсер для 'term' правил граматики. У нашому випадку - це цілі числа."""
+        token = self.current_token
+        self.eat(TokenType.INTEGER)
+        return Num(token)
+
+    def expr(self):
+        """ Парсер для арифметичних виразів. """
+        node = self.term()
+
+        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+            token = self.current_token
+            if token.type == TokenType.PLUS:
+                self.eat(TokenType.PLUS)
+            elif token.type == TokenType.MINUS:
+                self.eat(TokenType.MINUS)
+
+            node = BinOp(left=node, op=token, right=self.term())
+
+        return node
