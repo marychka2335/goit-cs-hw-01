8c160bca7643ef2e0958c63ca6b1b187bfe3d902
marychka2335 <marychka2335@icloud.com>
2024-04-06 23:43:48 +0300

    hw part2 commit

--- part2/lexer.py
+++ part2/lexer.py
@@ -1,6 +1,31 @@
 class LexicalError(Exception):
     pass
 
+
+class SyntaxError(Exception):
+    pass
+
+
+class TokenType:
+    INTEGER = "INTEGER"
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    MUL = "MUL"
+    DIV = "DIV"
+    LPAREN = "LPAREN"
+    RPAREN = "RPAREN"
+    EOF = "EOF"  # Означає кінець вхідного рядка
+
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.type}, {repr(self.value)})"
+
+
 class Lexer:
     def __init__(self, text):
         self.text = text
@@ -8,7 +33,7 @@ class Lexer:
         self.current_char = self.text[self.pos]
 
     def advance(self):
-        """ Переміщуємо 'вказівник' на наступний символ вхідного рядка """
+        """Переміщуємо 'вказівник' на наступний символ вхідного рядка"""
         self.pos += 1
         if self.pos > len(self.text) - 1:
             self.current_char = None  # Означає кінець введення
@@ -16,22 +41,21 @@ class Lexer:
             self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
-        """ Пропускаємо пробільні символи. """
+        """Пропускаємо пробільні символи."""
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
     def integer(self):
-        """ Повертаємо ціле число, зібране з послідовності цифр. """
-        result = ''
+        """Повертаємо ціле число, зібране з послідовності цифр."""
+        result = ""
         while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
             self.advance()
         return int(result)
 
     def get_next_token(self):
-        """ Лексичний аналізатор, що розбиває вхідний рядок на токени. """
+        """Лексичний аналізатор, що розбиває вхідний рядок на токени."""
         while self.current_char is not None:
-
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
@@ -39,14 +63,50 @@ class Lexer:
             if self.current_char.isdigit():
                 return Token(TokenType.INTEGER, self.integer())
 
-            if self.current_char == '+':
+            if self.current_char == "+":
                 self.advance()
-                return Token(TokenType.PLUS, '+')
+                return Token(TokenType.PLUS, "+")
 
-            if self.current_char == '-':
+            if self.current_char == "-":
                 self.advance()
-                return Token(TokenType.MINUS, '-')
+                return Token(TokenType.MINUS, "-")
+            
+            if self.current_char == "*":
+                self.advance()
+                return Token(TokenType.MUL, "*")
 
-            raise LexicalError('Помилка лексичного аналізу')
+            if self.current_char == "/":
+                self.advance()
+                return Token(TokenType.DIV, "/")
+
+            if self.current_char == "(":
+                self.advance()
+                return Token(TokenType.LPAREN, "(")
+
+            if self.current_char == ")":
+                self.advance()
+                return Token(TokenType.RPAREN, ")")
+            
+            raise LexicalError("Помилка лексичного аналізу")
 
         return Token(TokenType.EOF, None)
+
+
+def main():
+    while True:
+        try:
+            text = input('Введіть вираз (або "exit" для виходу): ')
+            if text.lower() == "exit":
+                print("Вихід із програми.")
+                break
+            lexer = Lexer(text)
+            token = lexer.get_next_token()
+            while token.type != TokenType.EOF:
+                print(token)
+                token = lexer.get_next_token()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
